94a26a37fa0a2a16820f691ade7ab24cd7bfebac
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2021-09-02 11:25:07 +0200

    extraer metodos
    extraer metodos en visual studio se hace con Control + .

--- Program.cs
+++ Program.cs
@@ -8,13 +8,18 @@ namespace PrimerosPasosCS
         {
             Console.WriteLine("Hola Mundo!");
 
+            calculateRadius();
+        }
+
+        private static void calculateRadius()
+        {
             Console.WriteLine("Welcome to a circunference area calculator. Insert the radius:");
             double radius = Convert.ToDouble(Console.ReadLine());
 
             Methods methods = new Methods();
 
             double area = methods.calcularAreaCircunferencia(radius);
-            Console.WriteLine("The area of the circunference is: {0}", area);     
+            Console.WriteLine("The area of the circunference is: {0}", area);
         }
     }
 }
